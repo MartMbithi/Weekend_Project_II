445cb5a3447b34456ca2cfc436fef16e3d6c69b2
Martin Mbithi <martdevelopers254@gmail.com>
2022-02-12 15:12:45 +0300

    Add Uniform Class Color Codes On Main Dashboard And Aside

---
+++ partials/aside.php
@@ -0,0 +1,84 @@
+<div class="sidenav" id="sidenav-main">
+    <!-- Sidenav header -->
+    <div class="sidenav-header d-flex align-items-center">
+        <a class="navbar-brand text-light" href="dashboard">
+            <img src="assets/img/brand/main_logo.png" class="navbar-brand-img" alt="...">
+            CPB MIS
+        </a>
+        <div class="ml-auto">
+            <!-- Sidenav toggler -->
+            <div class="sidenav-toggler sidenav-toggler-dark d-md-none" data-action="sidenav-unpin" data-target="#sidenav-main">
+                <div class="sidenav-toggler-inner">
+                    <i class="sidenav-toggler-line bg-white"></i>
+                    <i class="sidenav-toggler-line bg-white"></i>
+                    <i class="sidenav-toggler-line bg-white"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- User mini profile -->
+    <div class="sidenav-user d-flex flex-column align-items-center justify-content-between text-center">
+        <!-- Avatar -->
+        <div>
+            <a href="#" class="avatar rounded-circle avatar-xl">
+                <img alt="Image placeholder" src="../assets/img/theme/light/team-1-800x800.jpg" class="">
+            </a>
+            <div class="mt-4">
+                <h5 class="mb-0 text-white">Heather Parker</h5>
+                <span class="d-block text-sm text-white opacity-8 mb-3">Web Architect</span>
+                <a href="#" class="btn btn-sm btn-white btn-icon rounded-pill shadow hover-translate-y-n3">
+                    <span class="btn-inner--icon"><i class="far fa-coins"></i></span>
+                    <span class="btn-inner--text">$2.300</span>
+                </a>
+            </div>
+        </div>
+        <!-- User info -->
+        <!-- Actions -->
+        <div class="w-100 mt-4 actions d-flex justify-content-between">
+            <a href="user/profile.html" class="action-item action-item-lg text-white pl-0">
+                <i class="far fa-user"></i>
+            </a>
+            <a href="#modal-chat" class="action-item action-item-lg text-white" data-toggle="modal">
+                <i class="far fa-comment-alt"></i>
+            </a>
+            <a href="shop/invoices.html" class="action-item action-item-lg text-white pr-0">
+                <i class="far fa-receipt"></i>
+            </a>
+        </div>
+    </div>
+    <!-- Application nav -->
+    <div class="nav-application clearfix">
+        <a href="home.html" class="btn btn-square text-sm active">
+            <span class="btn-inner--icon d-block"><i class="far fa-home fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Home</span>
+        </a>
+        <a href="project/card-listing.html" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-project-diagram fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Projects</span>
+        </a>
+        <a href="task/table-listing.html" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-tasks fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Tasks</span>
+        </a>
+        <a href="task/kanban-board.html" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-columns fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Kanban</span>
+        </a>
+        <a href="user/card-listing.html" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-users-cog fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Users</span>
+        </a>
+        <a href="user/profile.html" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-user-ninja fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Profile</span>
+        </a>
+        <a href="shop/invoices.html" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-receipt fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Invoices</span>
+        </a>
+        <a href="widgets.html" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-cogs fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Widgets</span>
+        </a>
+    </div>
+</div>
