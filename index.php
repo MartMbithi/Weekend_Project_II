d9b7c2cc4815cfd0043319b46e7e37ccd2ae8031
Martin Mbithi <martdevelopers254@gmail.com>
2022-02-12 13:25:08 +0300

    Craft Login Pages UI

--- index.php
+++ index.php
@@ -13,12 +13,12 @@ require_once('partials/head.php');
                 <div class="min-vh-100 py-5 d-flex align-items-center">
                     <div class="w-100">
                         <div class="row justify-content-center">
-                            <div class="col-sm-10 col-lg-6">
+                            <div class="col-sm-8 col-lg-4">
                                 <div class="card shadow zindex-100 mb-0">
                                     <div class="card-body px-md-5 py-5">
                                         <div class="mb-5 text-center">
                                             <h6 class="h3">Login</h6>
-                                            <p class="text-muted  mb-0">Welcome to Cereals And Produce Board Information Management System</p>
+                                            <p class="text-muted  mb-0">Welcome to Uasin Gishu County Cereals And Produce Board Information Management System</p>
                                         </div>
                                         <span class="clearfix"></span>
                                         <form role="form" method="POST">
@@ -26,7 +26,7 @@ require_once('partials/head.php');
                                                 <label class="form-control-label">National ID Number</label>
                                                 <div class="input-group input-group-merge">
                                                     <div class="input-group-prepend">
-                                                        <span class="input-group-text"><i class="far fa-user-card"></i></span>
+                                                        <span class="input-group-text"><i class="far fa-user"></i></span>
                                                     </div>
                                                     <input type="text" name="user_idno" required class="form-control" id="input-email">
                                                 </div>
