eb31c5e869ca1f14655a356720b5c3ababe7c60f
Martin Mbithi <martdevelopers254@gmail.com>
2022-02-13 12:12:21 +0300

    All Unit Tests Pass On Update Operation

--- categories.php
+++ categories.php
@@ -109,7 +109,7 @@ require_once('partials/head.php');
                             <div class="modal-content">
                                 <div class="modal-header align-items-center">
                                     <div class="modal-title">
-                                        <h6 class="mb-0">Register New Prduct Category</h6>
+                                        <h6 class="mb-0">Register New Product Category</h6>
                                     </div>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
@@ -185,6 +185,50 @@ require_once('partials/head.php');
                                                 <a data-toggle="modal" href="#update_<?php echo $categories->category_id; ?>" class="badge badge-primary"><i class="fas fa-edit"></i> Edit</a>
                                                 <a data-toggle="modal" href="#delete_<?php echo $categories->category_id; ?>" class="badge badge-danger"><i class="fas fa-trash"></i> Delete</a>
                                             </td>
+                                            <!-- Update Modal -->
+                                            <div class="modal fade fixed-right" id="update_<?php echo $categories->category_id; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+                                                <div class="modal-dialog  modal-xl" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header align-items-center">
+                                                            <div class="modal-title">
+                                                                <h6 class="mb-0">Update <?php echo $categories->category_name; ?></h6>
+                                                            </div>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <form method="post" enctype="multipart/form-data" role="form">
+                                                                <div class="row">
+                                                                    <div class="form-group col-md-8">
+                                                                        <label for="">Name</label>
+                                                                        <input type="text" required name="category_name" value="<?php echo $categories->category_name; ?>" class="form-control" id="exampleInputEmail1">
+                                                                        <input type="hidden" required name="category_id" value="<?php echo $categories->category_id; ?>" class="form-control" id="exampleInputEmail1">
+                                                                    </div>
+                                                                    <div class="form-group col-md-4">
+                                                                        <label for="">Code</label>
+                                                                        <input type="text" readonly value="<?php echo $categories->category_code; ?>" required name="category_code" class="form-control">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="row">
+                                                                    <div class="form-group col-md-12">
+                                                                        <label for="exampleInputPassword1">Description</label>
+                                                                        <textarea required name="category_desc" rows="2" class="form-control"><?php echo $categories->category_desc; ?></textarea>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="text-right">
+                                                                    <button type="submit" name="update_category" class="btn btn-primary">Update Category</button>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- End Modal -->
+
+                                            <!-- Delete Modal -->
+
+                                            <!-- End Modal -->
                                         </tr>
                                     <?php
                                     }
