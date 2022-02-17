2b3a0aa666edd2abb06ccb5fb683edf04eaf0f06
Martin Mbithi <martdevelopers254@gmail.com>
2022-02-17 13:47:43 +0300

    Refactor Out Staffs - Nothing Like That

--- partials/aside.php
+++ partials/aside.php
@@ -44,66 +44,39 @@
     <!-- Application nav -->
     <div class="nav-application clearfix">
         <!-- Staff Side Menu -->
-        <?php if ($_SESSION['user_access_level'] == 'admin') { ?>
-            <a href="dashboard" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-home fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Home</span>
-            </a>
-            <a href="categories" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-project-diagram fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Categories</span>
-            </a>
-            <a href="products" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-tasks fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Products</span>
-            </a>
-            <a href="farmers" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-users-cog fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Farmers</span>
-            </a>
+        <a href="dashboard" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-home fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Home</span>
+        </a>
+        <a href="categories" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-project-diagram fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Categories</span>
+        </a>
+        <a href="products" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-tasks fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Products</span>
+        </a>
+        <a href="farmers" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-users-cog fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Farmers</span>
+        </a>
 
-            <a href="staffs" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-users fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Staffs</span>
-            </a>
-            <a href="invoices" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-receipt fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Invoices</span>
-            </a>
-            <a href="payments" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-hand-holding-usd fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Payments</span>
-            </a>
+        <a href="staffs" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-users fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Staffs</span>
+        </a>
+        <a href="invoices" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-receipt fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Invoices</span>
+        </a>
+        <a href="payments" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-hand-holding-usd fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Payments</span>
+        </a>
 
-            <a href="reports" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-file fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Reports</span>
-            </a>
-        <?php } else { ?>
-            <a href="dashboard" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-home fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Home</span>
-            </a>
-            <a href="categories" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-project-diagram fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Categories</span>
-            </a>
-            <a href="products" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-tasks fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Products</span>
-            </a>
-            <a href="farmers" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-users-cog fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Farmers</span>
-            </a>
-            <a href="invoices" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-receipt fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Invoices</span>
-            </a>
-            <a href="payments" class="btn btn-square text-sm">
-                <span class="btn-inner--icon d-block"><i class="far fa-hand-holding-usd fa-2x"></i></span>
-                <span class="btn-inner--icon d-block pt-2">Payments</span>
-            </a>
-        <?php } ?>
+        <a href="reports" class="btn btn-square text-sm">
+            <span class="btn-inner--icon d-block"><i class="far fa-file fa-2x"></i></span>
+            <span class="btn-inner--icon d-block pt-2">Reports</span>
+        </a>
     </div>
 </div>
